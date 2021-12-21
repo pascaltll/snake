5c1bd06268175c4198849d4ec76ac26de6bf3cbc
Карлос Туеста Льаха <71520076+pascaltll@users.noreply.github.com>
2021-12-21 16:10:19 +0300

    Delete cmake-build-release directory

--- cmake-build-release/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jc/CLionProjects/snake_v3/lib/googletest/googletest
-# Build directory: /home/jc/CLionProjects/snake_v3/cmake-build-release/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
