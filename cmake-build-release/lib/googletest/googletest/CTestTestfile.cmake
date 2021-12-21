69eb5cee389a7e278a7ab058f874da6f1a589fa9
pascaltll <tuesta.lx@phystech.edu>
2021-12-21 16:08:49 +0300

    correction 3

---
+++ cmake-build-release/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jc/CLionProjects/snake_v3/lib/googletest/googletest
+# Build directory: /home/jc/CLionProjects/snake_v3/cmake-build-release/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
